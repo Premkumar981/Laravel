8e281068ed8ee29404b8ffa6be8b3d6e6f17cdf2
Suru Prem Kumar <premkumarsuru@gmail.com>
2025-02-26 23:30:39 +0530

    created mult file

---
+++ resources/views/subt.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication</title>
+</head>
+<body>
+    <!-- <p>The multiplication of {{$num1}} and {{$num2}} is {{$num1-$num2}}</p>  -->
+    <!-- <br> -->
+
+    <p>Hey! This is third program in laravel trying to know syntax😉</p>
+    <?php
+    $sum=$num1-$num2;
+    echo "The multiplication of $num1 and $num2 is $sum";
+    ?>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/add/{num1}/{num2}', function ($num1,$num2) {
 Route::get('/mult/{num1}/{num2}', function ($num1,$num2) {
     return view('mult',['num1'=>$num1,'num2'=>$num2]);
 });
+
+// Subtraction of two numbers
+Route::get('/subt/{num1}/{num2}', function ($num1,$num2) {
+    return view('subt',['num1'=>$num1,'num2'=>$num2]);
+});
